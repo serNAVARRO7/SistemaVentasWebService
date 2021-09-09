2fcd99798537fb2d28af476ecbf737e6012aaef7
serNAVARRO <sernavarrops3@gmail.com>
2021-09-09 13:02:50 +0200

    Transacciones en Entity Framework

--- SistemaVentas/Controllers/VentaController.cs
+++ SistemaVentas/Controllers/VentaController.cs
@@ -25,25 +25,37 @@ namespace SistemaVentas.Controllers
             {
                 using (SistemaVentasContext db = new SistemaVentasContext())
                 {
-                    Venta oVenta = new Venta();
-                    oVenta.Total = oModel.Total;
-                    oVenta.Fecha = DateTime.Now;
-                    oVenta.IdCliente = oModel.IdCliente;
-                    db.Venta.Add(oVenta);
-                    db.SaveChanges();
-                    // En este momento Entity Framework genera un id para la venta
-                    foreach(var concepto in oModel.Conceptos)
+                    using (var transaction = db.Database.BeginTransaction())
                     {
-                        Models.Concepto oConcepto = new Models.Concepto();
-                        oConcepto.Cantidad = concepto.Cantidad;
-                        oConcepto.IdProducto = concepto.IdProducto;
-                        oConcepto.Importe = concepto.Importe;
-                        oConcepto.PrecioUnitario = concepto.PrecioUnitario;
-                        oConcepto.IdVenta = oVenta.Id;
-                        db.Concepto.Add(oConcepto);
-                        db.SaveChanges();
+                        try
+                        {
+                            Venta oVenta = new Venta();
+                            oVenta.Total = oModel.Conceptos.Sum(d => d.Cantidad * d.PrecioUnitario);
+                            oVenta.Fecha = DateTime.Now;
+                            oVenta.IdCliente = oModel.IdCliente;
+                            db.Venta.Add(oVenta);
+                            db.SaveChanges();
+                            // En este momento Entity Framework genera un id para la venta
+                            foreach (var concepto in oModel.Conceptos)
+                            {
+                                Models.Concepto oConcepto = new Models.Concepto();
+                                oConcepto.Cantidad = concepto.Cantidad;
+                                oConcepto.IdProducto = concepto.IdProducto;
+                                oConcepto.Importe = concepto.Importe;
+                                oConcepto.PrecioUnitario = concepto.PrecioUnitario;
+                                oConcepto.IdVenta = oVenta.Id;
+                                db.Concepto.Add(oConcepto);
+                                db.SaveChanges();
+                            }
+                            // La transaccion bloquea las tablas por lo que es necesario indicar que la transaccion ha finalizado
+                            transaction.Commit();
+                            oRespuesta.Exito = 1;
+                        }
+                        catch (Exception e)
+                        {
+                            transaction.Rollback();
+                        }
                     }
-                    oRespuesta.Exito = 1;
                 }
             }
             catch (Exception e)
