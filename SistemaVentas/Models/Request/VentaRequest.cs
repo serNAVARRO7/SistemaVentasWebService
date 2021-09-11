7495f7ad1d1f5b06d04a2ce3089c4bd398544416
serNAVARRO <sernavarrops3@gmail.com>
2021-09-11 18:04:52 +0200

    Validaciones de modelos

--- SistemaVentas/Models/Request/VentaRequest.cs
+++ SistemaVentas/Models/Request/VentaRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,14 @@ namespace SistemaVentas.Models.Request
 {
     public class VentaRequest
     {
+        [Required]
+        [Range(1, Double.MaxValue, ErrorMessage = "Valor de id incorrecto")]
+        [ExisteClienteAttributeValidation(ErrorMessage = "El cliente no existe")]
         public long IdCliente { get; set; }
         public decimal Total { get; set; }
 
+        [Required]
+        [MinLength(1, ErrorMessage = "Es necesario al menos un concepto")]
         public List<Concepto> Conceptos { get; set; }
 
         public VentaRequest()
@@ -25,4 +31,26 @@ namespace SistemaVentas.Models.Request
         public int Cantidad { get; set; }
         public decimal Importe { get; set; }
     }
+
+    #region Validaciones
+    public class ExisteClienteAttributeValidation : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int idCliente = (int)value;
+            using (SistemaVentasContext db = new SistemaVentasContext())
+            {
+                try
+                {
+                    if (db.Cliente.Find(idCliente) == null) return false;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Error durante la validacion del modelo con la BD");
+                }
+            }
+            return true;
+        }
+    }
+    #endregion
 }
